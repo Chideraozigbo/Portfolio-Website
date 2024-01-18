f2831e36261a18bb3528823e6136d85a76e02bbc
Chideraozigbo <chideraozigbo@gmail.com>
2024-01-18 09:39:54 +0100

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,135 @@
+import Layout from "@/components/Layout";
+import Head from "next/head";
+import Image from "next/image";
+import profile from "../../public/images/profile/dominic.png";
+import { useInView, useMotionValue, useSpring } from "framer-motion";
+import { useEffect, useRef } from "react";
+import Skills from "@/components/Skills";
+import Experience from "@/components/Experience";
+import Education from "@/components/Education";
+import AnimatedText from "@/components/AnimatedText";
+import TransitionEffect from "@/components/TransitionEffect";
+
+function AnimatedNumberFramerMotion({ value }) {
+  const ref = useRef(null);
+  const motionValue = useMotionValue(0);
+  const springValue = useSpring(motionValue, { duration: 3000 });
+  const isInView = useInView(ref, { once: true });
+  useEffect(() => {
+    if (isInView) {
+      motionValue.set(value);
+    }
+  }, [motionValue, value, isInView]);
+
+  useEffect(
+    () =>
+      springValue.on("change", (latest) => {
+        if (ref.current && latest.toFixed(0) <= value) {
+          ref.current.textContent = latest.toFixed(0);
+        }
+      }),
+    [springValue, value]
+  );
+
+  return <span ref={ref} />;
+}
+
+export default function About() {
+  return (
+    <>
+      <Head>
+        <title>About Chidera Ozigbo | About Page</title>
+        <meta name="description" content="Get to know the mind behind the data magic! Dive into my data engineering journey, exploring experiences, skills, and a passion for turning complex data challenges into streamlined solutions. 
+        Discover the expertise driving the transformation of raw data into actionable insights." />
+      </Head>
+      <TransitionEffect />
+      <main
+        className={`flex  w-full flex-col items-center justify-center dark:text-light`}
+      >
+        <Layout className="pt-16">
+          <AnimatedText
+            text="Drive Ignites Achievement!"
+            className="mb-16 !text-8xl !leading-tight lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8"
+          />
+
+          <div className="grid w-full grid-cols-8 gap-16 sm:gap-8">
+            <div className="col-span-3 flex flex-col items-start justify-start xl:col-span-4 md:order-2 
+            md:col-span-8">
+              <h2 className="mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75">
+                BIOGRAPHY
+              </h2>
+              <p className="font-medium ">
+                Hi, I&apos;m <strong>Chidera</strong>, a data engineer with a year of hands-on experience in the dynamic realm of data engineering. 
+                My journey has been fueled by a passion for transforming raw data into actionable insights that empower decision-making processes. 
+              </p>
+              <p className="my-4 font-medium">
+                My expertise extends to designing and implementing robust data architectures, optimizing performance, and ensuring data quality.
+                 
+                
+              </p>
+              <p className="font-medium">
+                I firmly believe in the power of data to drive informed business decisions, and I am excited about the prospect of bringing my skills to your team. 
+                
+              </p>
+            </div>
+            <div className="relative col-span-3 h-max rounded-2xl border-2 border-solid border-dark 
+            bg-light p-8 dark:border-light dark:bg-dark
+            xl:col-span-4 md:col-span-8 md:order-1
+            ">
+              <div
+                className="absolute  top-0 -right-3 -z-10 h-[103%] w-[102%]  rounded-[2rem] rounded-br-3xl 
+                bg-dark
+        dark:bg-light  "
+              />
+              <Image
+                className="h-auto w-full rounded-2xl"
+                src={profile}
+                alt="Chidera picture"
+                sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              33vw"
+                priority
+              />
+            </div>
+            <div className="col-span-2 flex flex-col items-end justify-between xl:col-span-8 xl:flex-row 
+            xl:items-center md:order-3">
+              <div className="flex flex-col items-end justify-center xl:items-center">
+                <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
+                  <AnimatedNumberFramerMotion value={5} />+
+                </span>
+                <h2 className="mb-4 text-xl font-medium capitalize text-dark/75 dark:text-light/75 
+                xl:text-center md:text-lg sm:text-base xs:text-sm">
+                  satisfied clients
+                </h2>
+              </div>
+
+              <div className="flex flex-col items-end justify-center xl:items-center">
+                <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
+                  <AnimatedNumberFramerMotion value={5} />+
+                </span>
+                <h2 className="mb-4 text-xl font-medium capitalize text-dark/75 dark:text-light/75 
+                xl:text-center md:text-lg sm:text-base xs:text-sm">
+                  projects completed
+                </h2>
+              </div>
+
+              <div className="flex flex-col items-end justify-center xl:items-center">
+                <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
+                  <AnimatedNumberFramerMotion value={1} />+
+                </span>
+                <h2 className="mb-4 text-xl font-medium capitalize text-dark/75 dark:text-light/75 
+                xl:text-center md:text-lg sm:text-base xs:text-sm">
+                  Year of experience
+                </h2>
+              </div>
+            </div>
+          </div>
+
+          <Skills />
+          <Experience />
+          <Education />
+        </Layout>
+      </main>
+    </>
+  );
+}
---
+++ src/pages/articles.js
@@ -0,0 +1,212 @@
+import AnimatedText from "@/components/AnimatedText";
+import { motion, useMotionValue } from "framer-motion";
+import Head from "next/head";
+import Image from "next/image";
+import blog1 from "../../public/images/articles/blog1.png";
+import blog2 from "../../public/images/articles/blog2.jpg";
+import blog3 from "../../public/images/projects/database.jpg";
+import blog4 from "../../public/images/articles/smooth scrolling in reactjs.png";
+import blog5 from "../../public/images/articles/create modal component in react using react portals.png";
+import blog6 from "../../public/images/articles/todo list app built using react redux and framer motion.png";
+import blog7 from "../../public/images/articles/What is Redux with easy explanation.png";
+import blog8 from "../../public/images/articles/What is higher order component in React.jpg";
+
+import Layout from "@/components/Layout";
+import Link from "next/link";
+import { useRef } from "react";
+import TransitionEffect from "@/components/TransitionEffect";
+
+const FramerImage = motion(Image);
+
+const MovingImg = ({ title, img, link }) => {
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+  const imgRef = useRef(null);
+
+  function handleMouse(event) {
+    imgRef.current.style.display = "inline-block";
+    x.set(event.pageX);
+    y.set(-10);
+  }
+
+  function handleMouseLeave(event) {
+    imgRef.current.style.display = "none";
+    x.set(0);
+    y.set(0);
+  }
+  return (
+    <>
+      <Link
+        href={link}
+        target={"_blank"}
+        className="relative"
+        onMouseMove={handleMouse}
+        onMouseLeave={handleMouseLeave}
+      >
+        <h2 className="capitalize text-xl font-semibold hover:underline dark:text-light md:text-lg xs:text-base">
+          {title}
+        </h2>
+        <FramerImage
+          src={img}
+          ref={imgRef}
+          alt={title}
+          className="w-96 h-auto z-10 hidden absolute rounded-lg md:!hidden"
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1, transition: { duration: 0.2 } }}
+          style={{
+            x: x,
+            y: y,
+          }}
+          sizes="(max-width: 768px) 60vw,
+              (max-width: 1200px) 40vw,
+              33vw"
+        />
+      </Link>
+    </>
+  );
+};
+
+const Article = ({ img, title, date, link }) => {
+  return (
+    <motion.li
+      initial={{ y: 200 }}
+      whileInView={{ y: 0, transition: { duration: 0.5, ease: "easeInOut" } }}
+      viewport={{ once: true }}
+      className="relative w-full p-4 py-6 my-2 rounded-xl flex sm:flex-col items-center justify-between 
+      bg-light text-dark first:mt-0 border border-solid border-dark
+      border-r-4 border-b-4 dark:bg-dark dark:border-light
+      "
+    >
+      <MovingImg img={img} title={title} link={link} />
+      <span className="text-primary font-semibold dark:text-primaryDark min-w-max pl-4 sm:self-start 
+      sm:pl-0 xs:text-sm">
+        {date}
+      </span>
+    </motion.li>
+  );
+};
+
+const FeaturedArticle = ({ img, title, time, summary, link }) => {
+  return (
+    <li className="relative w-full p-4 col-span-1 bg-light border border-dark border-solid rounded-2xl 
+    dark:bg-dark dark:border-light">
+      <div
+        className="absolute  top-0 -right-3 w-[102%] h-[103%] rounded-[2rem]  rounded-br-3xl bg-dark 
+        -z-10  "
+      />
+      <Link
+        href={link}
+        target={"_blank"}
+        className="inline-block rounded-lg overflow-hidden w-full"
+      >
+        <FramerImage
+          src={img}
+          alt={title}
+          className="w-full h-auto"
+          whileHover={{ scale: 1.05 }}
+          transition={{ duration: 0.2 }}
+          sizes="100vw"
+          priority
+        />
+      </Link>
+
+      <Link href={link} target={"_blank"}>
+        <h2 className="capitalize text-2xl font-bold my-2 mt-4 hover:underline xs:text-lg">
+          {title}
+        </h2>
+      </Link>
+      <p className="text-sm  mb-2">{summary}</p>
+      <span className="text-primary font-semibold dark:text-primaryDark">
+        {time}
+      </span>
+    </li>
+  );
+};
+
+export default function Articles() {
+  return (
+    <>
+      <Head>
+        <title>Articles On Data Engineering | Articles Page</title>
+        <meta name="description" content="Unlock the secrets of data engineering through insightful articles. 
+        Delve into topics spanning ETL strategies, ML pipeline best practices, and data modeling techniques. Stay informed, inspired, and empowered as we navigate the evolving landscape of data-driven innovation." />
+      </Head>
+      <TransitionEffect />
+      <main
+        className={`w-full mb-16 flex flex-col items-center justify-center dark:text-light overflow-hidden`}
+      >
+        <Layout className="pt-16">
+          <AnimatedText
+            text="Speech Shapes Global Impact!"
+            className="!text-8xl !leading-tight mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8"
+          />
+
+          <ul className="grid grid-cols-2 gap-16 lg:gap-8 md:grid-cols-1 md:gap-y-16">
+            <FeaturedArticle
+              img={blog1}
+              title="Data Analyst vs. Data Scientist: Understanding the Differences and Overlaps"
+              time="3 min read"
+              summary="Delve into the world of data roles as we dissect the variances and commonalities between Data Analysts and Data Scientists. 
+              Gain insights into their distinct skill sets, responsibilities, and how these roles synergize in the realm of data-driven decision-making."
+              link="https://medium.com/@chideraozigbo/data-analyst-vs-data-scientist-understanding-the-differences-and-overlaps-24bb91c0b67f"
+            />
+
+            <FeaturedArticle
+              img={blog2}
+              title="How to import data into a table in a database using MYSQL Command Line."
+              time="4 min read"
+              summary="Unlock the power of MYSQL Command Line as we guide you through the process of importing data into a database table. 
+              Learn effective strategies to troubleshoot and resolve common errors, ensuring a seamless data import experience."
+              link="https://medium.com/@chideraozigbo/how-to-import-data-into-a-table-in-a-database-using-mysql-command-line-and-solve-all-error-problems-89d2f9202300"
+            />
+          </ul>
+
+          <h2 className="font-bold text-4xl w-full text-center mt-32 my-16">
+            All Articles
+          </h2>
+
+          <ul className="flex flex-col items-center relative">
+            <Article
+              title="Database Design I: Employee Attrition Management System"
+              img={blog3}
+              date="December 25, 2023"
+              link="https://medium.com/@chideraozigbo/database-design-i-employee-attrition-management-system-25d89503c08b"
+            />
+            {/*
+            <Article
+              title="silky smooth scrolling in reactjs: a step-by-step guide for react developers"
+              img={blog4}
+              date="January 30, 2023"
+              link="https://devdreaming.com/blogs/smooth-scrolling-in-react-js"
+            />
+            <Article
+              title="creating an efficient modal component in react using hooks and portals"
+              img={blog5}
+              date="January 29, 2023"
+              link="https://devdreaming.com/blogs/create-efficient-modal-react-portals"
+            />
+            <Article
+              title="build a fabulous todo list app with react, redux and framer-motion"
+              img={blog6}
+              date="January 28, 2023"
+              link="https://devdreaming.com/blogs/build-react-redux-framer-motion-todo-app"
+            />
+            <Article
+              title="redux simplified: a beginner's guide for web developers"
+              img={blog7}
+              date="January 31, 2023"
+              link="https://devdreaming.com/blogs/redux-simply-explained"
+            />
+            <Article
+              title="what is higher order component (hoc) in react?"
+              date="January 4, 2023"
+              img={blog8}
+              link="https://devdreaming.com/blogs/higher-order-component-hoc-react"
+            />
+              */}
+          </ul>
+        </Layout>
+      </main>
+    </>
+  );
+}
