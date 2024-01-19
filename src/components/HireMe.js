c3ddacb7b5d37843e0c7ded3985224730ee18c06
Chideraozigbo <chideraozigbo@gmail.com>
2024-01-19 07:17:30 +0100

    update

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -1,13 +1,13 @@
 import Link from "next/link";
 import React from "react";
-import { CircularText } from "./Icons";
+import { CircularText, circular } from "./Icons";
 
 export const HireMe = () => {
   return (
     <div className="fixed left-4 bottom-4 flex flex-col items-center justify-center md:right-8 sm:right-0 
     overflow-hidden md:bottom-auto md:left-auto md:top-0 md:absolute z-20">
       <div className="w-48 h-auto flex items-center justify-center relative md:w-24">
-        <CircularText
+        <circular
           className={"fill-dark dark:fill-light animate-spin-slow duration-200"}
         />
         <Link
